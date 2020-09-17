6c921921cd9c5ce8c3b9642e80201ef767b87b08
Zixiao Chen <dev.chen.zixiao@dev.bwater.com>
2020-09-17 14:35:20 -0400

    retry

--- .circleci/circle_trigger.sh
+++ .circleci/circle_trigger.sh
@@ -13,18 +13,10 @@ URL="${CIRCLE_API}/v2/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CI
 
 
 echo -e "  $URL"
+echo -e "${CIRCLE_TOKEN}"
 
-HTTP_RESPONSE=$(curl --request POST \
-  --url https://circleci.com/api/v2/project/gh/fcorange/ngci-test/pipeline \
-  --header 'authorization: Basic 4ce8830073aefc635919d62306180dabf5411200' \
-  --header 'content-type: application/json' \
-  # --header 'x-attribution-actor-id: SOME_STRING_VALUE' \
-  # --header 'x-attribution-login: SOME_STRING_VALUE' \
-  --data '{"branch":"dev","parameters":{"test-project1":true}}')
+HTTP_RESPONSE=$(curl -s -u "${CIRCLE_TOKEN}:" -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" "$URL")
 
-
-
-# curl -s -u "${CIRCLE_API_USER_TOKEN}:" -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" "$URL")
 
 if [ "$HTTP_RESPONSE" -ge "200" ] && [ "$HTTP_RESPONSE" -lt "300" ]; then
     echo "API call succeeded."
