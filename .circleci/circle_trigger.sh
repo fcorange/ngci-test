10e13346590a075c1df03d4c1e83b816431ace4a
Zixiao Chen <dev.chen.zixiao@dev.bwater.com>
2020-09-18 08:47:07 -0400

    add git file change checker

--- .circleci/circle_trigger.sh
+++ .circleci/circle_trigger.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+# Constants
 REPOSITORY_TYPE="github"
 CIRCLE_API="https://circleci.com/api"
 CIRCLE_TOKEN="ca5b482eba36670b109cfe36cc80c19a1e1a6b92"
 
-PARAMETERS='"trigger-workflows":false, "test-project1":true'
+# Determine test targets
+CURRENT_COMMIT_HASH=$(git rev-parse HEAD)
+CHANGED_FILES=$(git diff-tree --no-commit-id --name-only -r ${CURRENT_COMMIT_HASH})
 
+if [[ $CHANGED_FILES == *"project1/"* ]]; then
+  TEST_PROJECT1=true
+fi
+
+if [[ $CHANGED_FILES == *"project2/"* ]]; then
+  TEST_PROJECT2=true
+fi
+
+PARAMETERS='"trigger-workflows":false, "test-project1":$TEST_PROJECT1, "test-project2":$TEST_PROJECT2'
+
+# Assemble POST data
 DATA="{ \"branch\": \"$CIRCLE_BRANCH\", \"parameters\": { $PARAMETERS } }"
 echo "Triggering pipeline with data:"
 echo -e "  $DATA"
 
 URL="${CIRCLE_API}/v2/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline"
 
-
-echo -e "  $URL"
-echo -e "${CIRCLE_TOKEN}"
-
+# POST to CircleCI API
 HTTP_RESPONSE=$(curl -s -u "${CIRCLE_TOKEN}:" -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" "$URL")
-
 
 if [ "$HTTP_RESPONSE" -ge "200" ] && [ "$HTTP_RESPONSE" -lt "300" ]; then
     echo "API call succeeded."
